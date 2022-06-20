0086f76b37a503606901fa80dcda2fc432f36abc
oomaar <omaar_5@hotmail.com>
2022-06-20 23:19:02 +0200

    describe the hashing and salting process

--- db/models/Users.js
+++ db/models/Users.js
@@ -24,6 +24,14 @@ userSchema.post("save", function (doc, next) {
 
 // Fire a function before doc saved to db
 // for more checkout the mongoose hooks docs
+
+// Hashing password process:-
+// ------------------------
+// 1) we take the user's password.
+// 2) add a salt to it (a salt is a string added to the password it's self).
+// 3) then we hash the salted password (this process generates a complex string).
+// 4) then it's stored to the database.
+// 5) hackers can reverse a simple hashing process so thats why we must add a salt to the password before hashing it.
 userSchema.pre("save", function (next) {
   console.log("user about to be created and saved", this);
 
