45c1c00cda4ae23f1cd213ecf1a17e91c08aac7a
oomaar <omaar_5@hotmail.com>
2022-06-22 09:34:25 +0200

    basic cookie set

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.user(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +29,9 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// cookies
+app.get("/set-cookies", (req, res) => {
+  res.setHeader("Set-Cookie", "newUser=true");
+  res.send("you had a cookie");
+});
